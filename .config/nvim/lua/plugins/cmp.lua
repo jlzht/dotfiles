a07b83cbf505a0db8686127af61cb39a7942841c
Saulo Amaral <jl.zht@proton.me>
2024-01-15 20:45:14 -0300

    fix: no snippet completion

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -4,6 +4,11 @@ local luasnip = require("luasnip")
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-j>'] = cmp.mapping.select_prev_item(),
     ['<C-k>'] = cmp.mapping.select_next_item(),
--- .config/nvim/lua/plugins/manager.lua
+++ .config/nvim/lua/plugins/manager.lua
@@ -23,7 +23,8 @@ return require("packer").startup(function(use)
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lua'
+    'hrsh7th/cmp-nvim-lua',
+    'saadparwaiz1/cmp_luasnip'
   }
   use {
     -- Snippet engine
